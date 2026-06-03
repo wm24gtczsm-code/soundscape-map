3c984639ea282601888a1f6a61bafa8f43316fed
宇野凛杜 <unorinto@MacBook-Pro-4.local>
2026-06-03 23:05:55 +0900

    fix

--- server.js
+++ server.js
@@ -260,6 +260,7 @@ app.post('/delete-request', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`http://localhost:${PORT}`);
 });
+
